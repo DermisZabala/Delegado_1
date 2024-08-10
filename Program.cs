5bb3f2729db8c05670aab233fbf9b1d2c49767c6
DermisZabala <dermiszabala@gmail.com>
2024-08-09 20:55:16 -0400

    Creacion de Delegado - LLamando a metodos de diferente clase

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Delegados_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Delegado_1 ElDelegado = new Delegado_1(Personas.DatosPersona);
+
+            Console.WriteLine(ElDelegado("Hamel", 19));
+            Console.WriteLine(ElDelegado("Juan", 30));
+
+            ElDelegado = new Delegado_1(Animal.DatosAnimal);
+
+            Console.WriteLine(ElDelegado("CHIVI", 3));
+            Console.WriteLine(ElDelegado("BOLA NEGRA", 5));
+
+
+            Console.ReadKey();
+
+        }
+
+        delegate string Delegado_1(string nombre, int edad);
+
+    }
+    class Personas
+    {
+        public static string DatosPersona(string nombre, int edad)
+        {
+            return "Nombre de la persona: " + nombre + " \tEdad: " + edad;
+        }
+    }
+    class Animal
+    {
+        public static string DatosAnimal(string nombre, int edad)
+        {
+            return "Nombre del animal: " + nombre + " \tEdad: " + edad;
+        }
+    }
+}
